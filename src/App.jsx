ca2d69af19e3c1846b30d66583b10cf77a575a99
Gabriela dos Santos <gabisinhas@yahoo.com.br>
2025-02-25 14:34:49 -0300

    react first commit.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function App() {
+  return (
+    <>
+  <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+      <Nav className="me-auto">
+          <Nav.Link href="#home">Create Employee</Nav.Link>
+          <Nav.Link href="#link">List All Employees</Nav.Link>
+      </Nav>
+      <Navbar.Collapse className="justify-content-end">
+          <Navbar.Brand href="#home">CRUD Vue.js + Node.js + PostgreSQL</Navbar.Brand>
+      </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+  )
+}
+
+export default App
