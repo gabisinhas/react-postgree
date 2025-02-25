8bf01644b084e8609c659fedaaf7a80e840ee5f0
Gabriela dos Santos <gabisinhas@yahoo.com.br>
2025-02-25 18:45:20 -0300

    new commit.

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import Navbar from 'react-bootstrap/Navbar';
 function App() {
   return (
     <>
-  <Navbar expand="lg" className="bg-body-tertiary">
+  <Navbar expand="lg" className="bg-body-tertiary"  bg="dark" data-bs-theme="dark">
       <Container>
       <Nav className="me-auto">
-          <Nav.Link to='/'>Create Employee</Nav.Link>
-          <Nav.Link to="/listEmployees">List All Employees</Nav.Link>
+          <Nav.Link href='/createemployee'>Create Employee</Nav.Link>
+          <Nav.Link href="/listEmployees">List All Employees</Nav.Link>
       </Nav>
       <Navbar.Collapse className="justify-content-end">
-          <Navbar.Brand href="#home">CRUD Vue.js + Node.js + PostgreSQL</Navbar.Brand>
+          <Navbar.Brand href="#home">CRUD React + Node.js + PostgreSQL</Navbar.Brand>
       </Navbar.Collapse>
       </Container>
     </Navbar>
