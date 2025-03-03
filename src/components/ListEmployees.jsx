74baa430790c17a71314a9bd080970b1b25f5865
Gabriela dos Santos <gabisinhas@yahoo.com.br>
2025-03-03 15:32:12 -0300

    new commit.

--- src/components/ListEmployees.jsx
+++ src/components/ListEmployees.jsx
@@ -1,42 +1,50 @@
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './NavBar';
-
+import { getAllEmployees } from '../services/Api';
 const ListEmployees = () => {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const data = await getAllEmployees();
+        setEmployees(data);
+      } catch (error) {
+        console.error('Error fetching employees:', error);
+      }
+    };
+
+    fetchEmployees();
+  }, []);
+
   return (
     <>
       <Header />
       <Container>
         <br />
         <h2>List Employees</h2>
-        <table class="border-collapse border border-gray-400 ...">
-        <thead>
-          <tr>
-            <th class="border border-gray-300 ...">Song</th>
-            <th class="border border-gray-300 ...">Artist</th>
-            <th class="border border-gray-300 ...">Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="border border-gray-300 ...">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td class="border border-gray-300 ...">Malcolm Lockyer</td>
-            <td class="border border-gray-300 ...">1961</td>
-          </tr>
-          <tr>
-            <td class="border border-gray-300 ...">Witchy Woman</td>
-            <td class="border border-gray-300 ...">The Eagles</td>
-            <td class="border border-gray-300 ...">1972</td>
-          </tr>
-          <tr>
-            <td class="border border-gray-300 ...">Shining Star</td>
-            <td class="border border-gray-300 ...">Earth, Wind, and Fire</td>
-            <td class="border border-gray-300 ...">1975</td>
-          </tr>
-        </tbody>
-      </table>
+        <table className="border-separate border-spacing-2 border border-gray-400 dark:border-gray-500">
+          <thead>
+            <tr>
+              <th className="border border-gray-300 ...">Name</th>
+              <th className="border border-gray-300 ...">Job Role</th>
+              <th className="border border-gray-300 ...">Employee Registration</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee) => (
+              <tr key={employee.id}>
+                <td className="border border-gray-300 ...">{employee.name}</td>
+                <td className="border border-gray-300 ...">{employee.job_role}</td>
+                <td className="border border-gray-300 ...">{employee.employee_registration}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default ListEmployees
+export default ListEmployees;
