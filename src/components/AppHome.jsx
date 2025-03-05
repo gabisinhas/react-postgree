fe8a2458bad9e939428386df92b3e32e872aef8f
Gabriela dos Santos <65128340+gabisinhas@users.noreply.github.com>
2025-03-05 15:17:22 -0300

    commit.

--- src/components/AppHome.jsx
+++ src/components/AppHome.jsx
@@ -6,7 +6,7 @@ const AppHome = () => {
     <>
       <Header />
       <div style={{ paddingLeft: '14em', paddingTop: '4em'}}>
-          <h2>Bem Vindo a Minha Primeira Página Usando React!!!</h2>
+          <h2 className="text-indigo-800">Bem Vindo a Minha Primeira Página Usando React!!!</h2>
           <img src="src/assets/private-public-routing-react.png" alt="Descrição da imagem" width="700" height="300" />
       </div>
       <div style={{ paddingTop: '1em'}}>
--- src/components/ListEmployees.jsx
+++ src/components/ListEmployees.jsx
@@ -6,16 +6,24 @@ const ListEmployees = () => {
   const [employees, setEmployees] = useState([]);
 
   useEffect(() => {
-    const fetchEmployees = async () => {
-      try {
-        const data = await getAllEmployees();
-        setEmployees(data);
-      } catch (error) {
-        console.error('Error fetching employees:', error);
-      }
-    };
+    // const fetchEmployees = async () => {
+    //   try {
+    //     const data = await getAllEmployees();
+    //     setEmployees(data);
+    //   } catch (error) {
+    //     console.error('Error fetching employees:', error);
+    //   }
+    // };
 
-    fetchEmployees();
+    // fetchEmployees();
+
+    // Dados de exemplo
+    const sampleEmployees = [
+      { id: 1, name: 'John Doe', job_role: 'Developer', employee_registration: '12345' },
+      { id: 2, name: 'Jane Smith', job_role: 'Designer', employee_registration: '67890' },
+      { id: 3, name: 'Alice Johnson', job_role: 'Manager', employee_registration: '11223' },
+    ];
+    setEmployees(sampleEmployees);
   }, []);
 
   return (
@@ -23,13 +31,13 @@ const ListEmployees = () => {
       <Header />
       <Container>
         <br />
-        <h2>List Employees</h2>
+        <h2 className="text-indigo-800">List Employees</h2>
         <table className="border-separate border-spacing-2 border border-gray-400 dark:border-gray-500">
           <thead>
             <tr>
-              <th className="border border-gray-300 ...">Name</th>
-              <th className="border border-gray-300 ...">Job Role</th>
-              <th className="border border-gray-300 ...">Employee Registration</th>
+              <th className="bg-gray-400 border border-gray-300 ...">Name</th>
+              <th className="bg-gray-400 border border-gray-300 ...">Job Role</th>
+              <th className="bg-gray-400 border border-gray-300 ...">Employee Registration</th>
             </tr>
           </thead>
           <tbody>
