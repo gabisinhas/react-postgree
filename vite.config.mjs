7f72207a9e7424e557f2a68a79d269d7762972c4
Gabriela dos Santos <gabisinhas@yahoo.com.br>
2025-03-06 10:00:35 -0300

    commit.

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+  },
   plugins: [
     react(),
     tailwindcss()
